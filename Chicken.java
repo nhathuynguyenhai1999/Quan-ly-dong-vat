79f33083b42632c5c14e8240d551b988dd8c79d7
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-01-24 11:41:41 +0700

    upload my files
    Để dễ dàng gọi (sử dụng) phương thức makeSound() của các đối tượng mô phỏng động, tạo lớp trừu tượng Animal.
    
    Triển khai lớp Chicken và Tiger là lớp con của lớp Animal.
    
    Để dễ dàng gọi (sử dụng) phương thức howToEat() của các đối tượng “ăn được”, tạo interface Edible. Cho lớp Chicken triển khai interface Edible.
    
    Tạo lớp trừu tượng Fruit để mô phỏng các đối tượng “trái cây”, cũng là một Edible. Triển khai các lớp con Orange và Apple.

---
+++ Chicken.java
@@ -0,0 +1,19 @@
+package animal;
+
+import animal.edible.Edible;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+    @Override
+    public String howToEat() {
+        return "could be fried";
+    }
+
+    @Override
+    public String howtoEat() {
+        return null;
+    }
+}
---
+++ Tiger.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+
+    @Override
+    public String howToEat() {
+        return null;
+    }
+}
